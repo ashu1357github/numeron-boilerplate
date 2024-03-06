357cd2c98c6f149d9c12230c7ba7cb16e69e1309
ashu <ashupatil1357@gmail.com>
2024-03-06 14:23:15 +0530

    code updated

--- score.js
+++ score.js
@@ -1,1 +1,6 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score44=localStorage.getItem("gameScore");
+document.getElementById("score-board").innerText=score44;
+document.getElementById("play-again-button").onclick=()=>{
+    location.href="./game.html";
+}
